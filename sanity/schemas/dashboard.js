b74682b03e2b7a216048921a0f65ce9386aab2eb
mikehwagz <mike@selfaware.studio>
2019-10-20 16:45:04 -0400

    feat(sanity): add netlify dashboard plugin

---
+++ sanity/schemas/dashboard.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: 'netlify',
+      options: {
+        title: 'Netlify',
+        sites: [
+          {
+            title: 'Website',
+            apiId: 'c3a71644-a48b-426d-8627-071e4e719e0f',
+            buildHookId: '5d71d74bdf65f46039c157a6',
+            name: 'rebees',
+          },
+        ],
+      },
+    },
+  ],
+}
