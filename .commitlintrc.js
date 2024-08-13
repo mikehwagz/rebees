c4afe1d3f674a1c7bf3badfde8c4fca87f036557
Mike Wagz <mike@selfaware.studio>
2024-08-13 14:54:29 -0400

    chore(repo): update commitlint config

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-empty': [2, 'never'],
-    'scope-enum': [2, 'always', ['repo', 'app', 'sanity']],
+    'scope-enum': [2, 'always', ['repo', 'app', 'studio']],
   },
 }
