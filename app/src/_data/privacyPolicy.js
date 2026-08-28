14b1cddf9dd83450d4823e002e9f60fc2b9639c6
Aaron Lopez <aaron@rftb.agency>
2026-08-28 13:09:57 -0500

    fix(app): privacy policy styled h2 and h3

--- app/src/_data/privacyPolicy.js
+++ app/src/_data/privacyPolicy.js
@@ -2,6 +2,28 @@ const client = require('../util/client')
 const groq = require('groq')
 const blocksToHtml = require('@sanity/block-content-to-html')
 
+const h = blocksToHtml.h
+
+function blockSerializer(props) {
+  const style = props.node.style || 'normal'
+
+  if (style === 'h2') {
+    return h('h2', { className: 'f30 s:f46' }, props.children)
+  }
+
+  if (style === 'h3') {
+    return h('h3', { className: 'f22 s:f30' }, props.children)
+  }
+
+  if (/^h\d/.test(style)) {
+    return h(style, null, props.children)
+  }
+
+  return style === 'blockquote'
+    ? h('blockquote', null, props.children)
+    : h('p', null, props.children)
+}
+
 module.exports = async function() {
   const privacyPolicy = await client.fetch(
     groq`
@@ -25,6 +47,9 @@ module.exports = async function() {
   if (privacyPolicy.content && privacyPolicy.content.length) {
     privacyPolicy.content = blocksToHtml({
       blocks: privacyPolicy.content.filter(Boolean),
+      serializers: {
+        types: { block: blockSerializer },
+      },
     })
   }
   return privacyPolicy
